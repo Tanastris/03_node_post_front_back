67582f1053d330b1c29f7462f91c7a45f2746452
RokasV <vezikauskas.r@gmail.com>
2023-12-15 08:37:27 +0200

    server code

--- src/server.js
+++ src/server.js
@@ -1,2 +1,15 @@
-"use strict";
-console.log("server.js file was loaded");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+
+// Routes
+
+// app Listen
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});

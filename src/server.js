d41deb887e5ae9cb08d5b689dc11623dda94c3c4
RokasV <vezikauskas.r@gmail.com>
2023-12-15 11:08:56 +0200

    single post

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const morgan = require("morgan");
 const cors = require("cors");
-const config = require("./config");
+const morgan = require("morgan");
+
+const { port } = require("./config");
 const app = express();
 
 // Database
@@ -16,35 +17,39 @@ const postsArr = [
     id: 2,
     title: "Post 2",
     content: "Body of post 2",
-    date: "2020-02-02",
+    date: "2020-01-02",
   },
   {
     id: 3,
     title: "Post 3",
     content: "Body of post 3",
-    date: "2020-03-03",
+    date: "2020-01-03",
   },
   {
     id: 4,
     title: "Post 4",
     content: "Body of post 4",
-    date: "2020-04-04",
+    date: "2020-01-04",
   },
 ];
 
 let lastPostId = 4;
+
 // Middleware
-app.use(cors());
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(cors());
+app.use(express.json()); // for parsing application/json
 
 // Routes
-app.post("/posts", (req, res) => {
-  const newPostObj = req.body;
-  console.log(req.body);
+app.post("/posts", (request, response) => {
+  const newPostObj = request.body;
+  console.log(newPostObj);
   // newPostObj.id = ++lastPostId;
-  postsArr.push({ id: ++lastPostId, ...newPostObj });
-  res.json(postsArr);
+  postsArr.push({
+    id: ++lastPostId,
+    ...newPostObj,
+  });
+  response.json(postsArr);
 });
 
 app.get("/", (req, res) => {
@@ -55,16 +60,30 @@ app.get("/", (req, res) => {
 app.get("/posts", (req, res) => {
   res.json(postsArr);
 });
-// GET - /posts/dates - grazins visus postus
+// GET - /posts?id=5 - grazins post su id 5
+
+app.get("/posts/:postId", (req, res) => {
+  const postId = +req.params.postId;
+  console.log(req.params);
+  // surasti posta mus postArr kurio id === postId
+  const found = postsArr.find((pObj) => pObj.id === postId);
+  if (found === undefined) {
+    res.status(404).json({
+      msg: `post with id: ${postId} was not found`,
+    });
+    return;
+  }
+  res.json(found);
+});
+
+// GET - posts/dates - grazins visas postu datas masyve
 app.get("/posts/dates", (req, res) => {
   const datesArr = postsArr.map((pObj) => pObj.date);
+  console.log("datesArr ===", datesArr);
   res.json(datesArr);
 });
 
-// Get - posts/dates - grazins visas postu datas masyve
-
 // app Listen
-
-app.listen(config.port, () => {
-  console.log(`server is running on port ${config.port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });

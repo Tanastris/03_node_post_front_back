21050e9c2adb89e9a8f28f2e0f1ce86e871c8210
RokasV <vezikauskas.r@gmail.com>
2023-12-15 08:54:23 +0200

    create server + rest + config

--- src/server.js
+++ src/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-
+const config = require("./config");
 const app = express();
 
 // Middleware
 
+app.use(morgan("common"));
+
 // Routes
+
+app.get("/", (req, res) => {
+  res.json({ msg: "home route" });
+});
 
 // app Listen
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+app.listen(config.port, () => {
+  console.log(`server is running on port ${config.port}`);
 });
